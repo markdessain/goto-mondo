2c23ae8c71cc568ac31dd03a7e444b0eba8acb34
Mark Dessain <mark.dessain@gmail.com>
2016-01-25 15:19:08 +0000

    Update the settings

--- python/settings.py
+++ python/settings.py
@@ -5,6 +5,7 @@ import redis
 
 
 def get_env():
+    # Load the settings from the .env file if no environment has been selected
     if not os.environ.get('ENV'):
         a = os.path.join(os.path.dirname(__file__), '../.env')
         with open(a) as f:
@@ -16,12 +17,17 @@ def get_env():
 
 env = get_env()
 
-logging.basicConfig(level=getattr(logging, env.get('LOG_LEVEL').upper()))
+log_level = env.get('LOG_LEVEL').upper()
+logging.basicConfig(level=getattr(logging, log_level))
+flask_debug = True if log_level == 'debug' else False
 
 redis_client = redis.from_url(env.get('REDIS_URL'))
 
 mondo_env = env.get('MONDO_ENV')
-mondo_visit_count = env.get('MONDO_VISIT_COUNT')
+mondo_visit_count = int(env.get('MONDO_VISIT_COUNT'))
+
+# allows four accounts to be hard coded in the environment
+# TODO - Use OAuth
 mondo_account_mapping = dict([env.get('MONDO_ACCOUNT_%s' % i).split(':') for i in range(1, 5)])
 
 foursquare_client_id = env.get('FOURSQUARE_CLIENT_ID')
