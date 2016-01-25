7b87f398d3b014111acaba612218726f101cab75
Mark Dessain <mark.dessain@gmail.com>
2016-01-25 15:43:13 +0000

    Tidy up the main splitting out the code into models

--- python/main.py
+++ python/main.py
@@ -1,16 +1,13 @@
 import json
 import logging
-import datetime
 
 from flask import Flask, render_template, request
-from settings import redis_client, mondo_visit_count, page_url
 
-from utils import foursquare
-from utils import mondo
-from models import Suggestion
+import settings
+from models import Suggestion, Transaction
 
 app = Flask(__name__, template_folder='../html', static_folder='../static')
-app.config['DEBUG'] = True
+app.config['DEBUG'] = settings.flask_debug
 
 
 log = logging.getLogger(__name__)
@@ -29,51 +26,23 @@ def route_suggestion(suggestion_id):
 
 @app.route('/webhook', methods=['POST'])
 def route_webhook():
-    transaction = json.loads(request.data.decode('utf8'))
+    data = json.loads(request.data.decode('utf8'))
 
-    if not transaction['data']['is_load']:
+    if data['type'] == 'transaction.created':
+        transaction = Transaction(data['data'])
+        transaction.save()
 
-        merchant = transaction['data']['merchant']
-        account_id = transaction['data']['account_id']
+        if transaction.visited_count > settings.mondo_visit_count:
+            suggestion = transaction.find_suggestion()
 
-        date = datetime.date.today()
-        year_and_week = "%s_%s" % (date.isocalendar()[1], date.year)
+            if suggestion:
+                suggestion.save()
+                suggestion.post_to_feed()
 
-        redis_key = "%s_%s_%s" % (account_id, merchant['id'], year_and_week)
-        redis_client.incr(redis_key, 1)
-
-        current_count = redis_client.get(redis_key)
-        if int(current_count) > int(mondo_visit_count):
-            name = merchant['name']
-            body = "You've already been to %s %s times this week. Why try here instead?" % (name, int(current_count))
-            long = merchant['address']['longitude']
-            lat = merchant['address']['latitude']
-
-            venue_id = foursquare.get_venue_id(name, lat, long)
-
-            if venue_id:
-                similar_venues = foursquare.get_similar_venues(venue_id)
-
-                if similar_venues['items']:
-                    new_name = similar_venues['items'][0]['name']
-                    title = 'Try %s next time?' % new_name
-                    image_url = similar_venues['items'][0]['categories'][0]['icon']['prefix'] + 'bg_64' + similar_venues['items'][0]['categories'][0]['icon']['suffix']
-
-                    suggestion_id = Suggestion(new_name, name, current_count).save()
-                    url = '%s/suggestion/%s' % (page_url, suggestion_id)
-                    mondo.post_to_feed(account_id, title, body, url, image_url)
-                else:
-                    log.info('Nothing Similar')
-            else:
-                log.info('Could not find the venue')
-        else:
-            log.info('Only been once')
     else:
-        log.info('Mondo Top up')
+        log.warning('Unsupported webhook type: %s' % data['type'])
 
     return ''
 
-
 if __name__ == "__main__":
     app.run()
-
--- python/models.py
+++ python/models.py
@@ -1,22 +1,99 @@
 import json
+import logging
+import datetime
 
-from utils.strings import random_string
+import settings
+from utils import mondo
+from utils import foursquare
 from settings import redis_client
+from utils.strings import random_string
 
 
-class Suggestion(object):
+log = logging.getLogger(__name__)
 
-    def __init__(self, name, replacement, count):
+
+class Model(object):
+    pass
+
+
+class Suggestion(Model):
+
+    def __init__(self, account_id, image_url, name, replacement, count):
+        self.account_id = account_id
+        self.image_url = image_url
         self.name = name
         self.replacement = replacement
-        self.count = count
+        self.count = int(count)
+        self.redis_key = ''
 
     def save(self):
-        name = random_string()
-        redis_client.set('suggestion:%s' % name, json.dumps({'name': self.name, 'replacement': self.replacement, 'count': int(self.count) }))
-        return name
+        redis_key = 'suggestion:%s' % random_string()
+        data = {
+            'account_id': self.account_id,
+            'image_url': self.image_url,
+            'name': self.name,
+            'replacement': self.replacement,
+            'count': self.count,
+        }
+        redis_client.set(redis_key, json.dumps(data))
+        self.redis_key = redis_key
+        return redis_key
 
     @staticmethod
-    def get(name):
-        values = json.loads(redis_client.get('suggestion:%s' % name).decode('utf-8'))
+    def get(redis_key):
+        values = json.loads(redis_client.get(redis_key).decode('utf-8'))
         return Suggestion(**values)
+
+    def post_to_feed(self):
+        title = 'Try %s next time?' % self.name
+        body = "You've already been to %s %s times this week. Why try here instead?" % (self.replacement, self.count)
+
+        url = '%s/suggestion/%s' % (settings.page_url, self.redis_key)
+        mondo.post_to_feed(self.account_id, title, body, url, self.image_url)
+
+
+class Transaction(Model):
+
+    def __init__(self, data):
+        self.data = data
+        self.redis_key = ''
+
+    def save(self):
+        merchant = self.data['merchant']
+        account_id = self.data['account_id']
+
+        date = datetime.date.today()
+        year_and_week = '%s_%s' % (date.isocalendar()[1], date.year)
+
+        redis_key = '%s_%s_%s' % (account_id, merchant['id'], year_and_week)
+        redis_client.incr(redis_key, 1)
+
+        self.redis_key = redis_key
+        return redis_key
+
+    @property
+    def visited_count(self):
+        return int(redis_client.get(self.redis_key))
+
+    def find_suggestion(self):
+        merchant = self.data['merchant']
+        account_id = self.data['account_id']
+
+        name = merchant['name']
+        long = merchant['address']['longitude']
+        lat = merchant['address']['latitude']
+
+        venue_id = foursquare.get_venue_id(name, lat, long)
+
+        if venue_id:
+            similar_venues = foursquare.get_similar_venues(venue_id)
+
+            if similar_venues['items']:
+                new_name = similar_venues['items'][0]['name']
+                image_url = similar_venues['items'][0]['categories'][0]['icon']['prefix'] + 'bg_64' + similar_venues['items'][0]['categories'][0]['icon']['suffix']
+
+                return Suggestion(account_id, image_url, new_name, name, self.visited_count)
+            else:
+                log.info('No suggestion available')
+        else:
+            log.info('Could not find the venue')
